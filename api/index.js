cb375d1cb0374381b173f30135dd6c1cc91a21aa
MAFIAMATA <maiquelpuma91@gmail.com>
2025-05-07 21:50:50 -0400

    mmm

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const serverlessExpress = require('@vendia/serverless-express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+module.exports.handler = serverlessExpress({ app });
 
 app.get("/", (req, res) => {
   res.send("¡Hola desde la API REST!");
